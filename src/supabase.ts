c501299272159c2798c63fd01e9984f9a3a56088
r <100163255+r0b0roach@users.noreply.github.com>
2026-02-26 13:45:13 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -7,8 +7,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env['SUPABASE_URL'] || '';
-const supabaseKey = process.env['SUPABASE_KEY'] || '';
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
 
 if (!supabaseUrl || !supabaseKey) {
   console.warn(
