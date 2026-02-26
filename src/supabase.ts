80147b261f1b99c978e31755aafefef7604ee08f
r <100163255+r0b0roach@users.noreply.github.com>
2026-02-27 02:12:46 +0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -6,6 +6,9 @@
  */
 
 import { createClient } from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL || '';
 const supabaseKey = process.env.SUPABASE_KEY || '';
